f46a9c8574398f0f4818454b52ace3301298985b
ngocthach <vietnam120793@gmail.com>
2022-09-01 11:09:34 +0700

    first test

--- integration-tests/rs/src/tests.rs
+++ integration-tests/rs/src/tests.rs
@@ -1,129 +1,67 @@
 use near_units::parse_near;
 use serde_json::json;
 use workspaces::prelude::*;
-use workspaces::{network::Sandbox, sandbox, Account, Contract, Worker};
+use workspaces::{network::Sandbox, testnet, sandbox, Account, Contract, Worker};
 
-const WASM_FILEPATH: &str = "../../out/main.wasm";
+const WASM_FILEPATH: &str = "../../../out/contract.wasm";
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-  let sandbox = sandbox().await?;
+  let worker = sandbox().await?;
   let wasm = std::fs::read(WASM_FILEPATH)?;
-  let contract = sandbox.dev_deploy(&wasm).await?;
+  let contract = worker.dev_deploy(&wasm).await?;
 
   // create accounts
-  let owner = sandbox.root_account().unwrap();
+  let owner = worker.root_account().unwrap();
   let user = owner
-    .create_subaccount(&sandbox, "alice")
+    .create_subaccount(&worker, "near-ecommerce-payment-contract")
     .initial_balance(parse_near!("30 N"))
     .transact()
     .await?
     .into_result()?;
 
-  test_increment(&user, &contract, &sandbox).await?;
-  test_decrement(&user, &contract, &sandbox).await?;
-  test_reset(&user, &contract, &sandbox).await?;
+  let ft_contract_id = String::from("ft.vbidev.testnet");
+
+  let _create_contract = user
+      .call(&worker, contract.id(), "new")
+      .args_json(json!({"owner_id": owner.id(), "ft_contract_id": ft_contract_id}))?
+      .transact()
+      .await?
+      .json()?;
+
+  test_pay_order(&owner, &user, &contract, &worker).await?;
+  //test_pay_order(&user, &contract, &sandbox).await?;
+  //test_refund_order(&user, &contract, &sandbox).await?;
 
   Ok(())
 }
 
-async fn test_increment(
+async fn test_pay_order(
+  caller: &Account,
   user: &Account,
   contract: &Contract,
-  sandbox: &Worker<Sandbox>,
+  worker: &Worker<Sandbox>,
 ) -> anyhow::Result<()> {
-  let start_counter: u64 = user
-    .call(&sandbox, contract.id(), "get_num")
-    .args_json(json!({}))?
-    .transact()
-    .await?
-    .json()?;
 
-  user
-    .call(&sandbox, contract.id(), "increment")
-    .args_json(json!({}))?
-    .transact()
-    .await?;
+  // before transfer
+  let contract_balance = user
+      .view_account(&worker)
+      .await?
+      .balance;
 
-  let end_counter: u64 = user
-    .call(&sandbox, contract.id(), "get_num")
-    .args_json(json!({}))?
-    .transact()
-    .await?
-    .json()?;
-
-  assert_eq!(end_counter, start_counter + 1);
-  println!("Increment ✅");
-  Ok(())
-}
-
-async fn test_decrement(
-  user: &Account,
-  contract: &Contract,
-  sandbox: &Worker<Sandbox>,
-) -> anyhow::Result<()> {
-  user
-    .call(&sandbox, contract.id(), "increment")
-    .args_json(json!({}))?
+  caller
+    .call(&worker, contract.id(), "pay_order")
+    .args_json(json!({"order_id": "order_1", "order_amount": "1000000000000000000000000"}))?
     .transact()
     .await?;
 
-  let start_counter: u64 = user
-    .call(&sandbox, contract.id(), "get_num")
-    .args_json(json!({}))?
-    .transact()
-    .await?
-    .json()?;
+  // after transfer
+  let new_contract_balance = user
+      .view_account(&worker)
+      .await?
+      .balance;
 
-  user
-    .call(&sandbox, contract.id(), "decrement")
-    .args_json(json!({}))?
-    .transact()
-    .await?;
-
-  let end_counter: u64 = user
-    .call(&sandbox, contract.id(), "get_num")
-    .args_json(json!({}))?
-    .transact()
-    .await?
-    .json()?;
-
-  assert_eq!(end_counter, start_counter - 1);
-  println!("Decrement ✅");
-  Ok(())
-}
-
-async fn test_reset(
-  user: &Account,
-  contract: &Contract,
-  sandbox: &Worker<Sandbox>,
-) -> anyhow::Result<()> {
-  user
-    .call(&sandbox, contract.id(), "increment")
-    .args_json(json!({}))?
-    .transact()
-    .await?;
-
-  user
-    .call(&sandbox, contract.id(), "increment")
-    .args_json(json!({}))?
-    .transact()
-    .await?;
-
-  user
-    .call(&sandbox, contract.id(), "reset")
-    .args_json(json!({}))?
-    .transact()
-    .await?;
-
-  let end_counter: u64 = user
-    .call(&sandbox, contract.id(), "get_num")
-    .args_json(json!({}))?
-    .transact()
-    .await?
-    .json()?;
-
-  assert_eq!(end_counter, 0);
-  println!("Reset ✅");
+  assert_eq!(new_contract_balance, contract_balance + 1000000000000000000000000);
+  println!("Pay order successfully transferred to contract account {} ✅", contract.id());
   Ok(())
 }
